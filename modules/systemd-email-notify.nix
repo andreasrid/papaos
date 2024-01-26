81c0a81d7e901870c5137e0e3a0d73080f375559
Andreas Rid <info@andreas-rid.de>
2024-01-26 15:53:17 +0100

    add module systemd-email-notify

---
+++ modules/systemd-email-notify.nix
@@ -0,0 +1,67 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.systemd.email-notify;
+
+  sendmail = pkgs.writeScript "sendmail"
+    ''
+      #!/bin/sh
+
+      ${pkgs.system-sendmail}/bin/sendmail -t <<ERRMAIL
+      To: $1
+      From: ${config.systemd.email-notify.mailFrom}
+      Subject: Status of service $2
+      Content-Transfer-Encoding: 8bit
+      Content-Type: text/plain; charset=UTF-8
+
+      $(systemctl status --full "$2")
+      ERRMAIL
+    '';
+in
+{
+  options = {
+    systemd.email-notify = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Whether to enable Systemd Service Failure Notification System.
+        '';
+      };
+
+      mailTo = mkOption {
+        type = types.str;
+        default = null;
+        description = "Email address to which the service status will be mailed.";
+      };
+
+      mailFrom = mkOption {
+        type = types.str;
+        default = null;
+        description = "Email address from which the service status will be mailed.";
+      };
+    };
+
+    systemd.services = mkOption {
+      type = with types; attrsOf (
+        submodule {
+          config.onFailure = [ "email-notify@%n.service" ];
+        }
+      );
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services."email-notify@" = {
+      description = "Sends a status mail via sendmail on service failures.";
+      onFailure = mkForce [ ];
+      serviceConfig = {
+        ExecStart = "${sendmail} ${config.systemd.email-notify.mailTo} %i";
+        Type = "oneshot";
+      };
+    };
+  };
+
+}
