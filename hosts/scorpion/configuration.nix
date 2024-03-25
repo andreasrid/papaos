19138e4253fa06c88b5a4e3d494fd621d5908004
Andreas Rid <info@andreas-rid.de>
2024-03-25 17:53:36 +0100

    scorpion: disable GNOME Desktop Environment

--- hosts/scorpion/configuration.nix
+++ hosts/scorpion/configuration.nix
@@ -39,8 +39,8 @@
 
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  #services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
   
 
   # Configure keymap in X11
