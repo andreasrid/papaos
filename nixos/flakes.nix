1201e0ed91a2c347f832593dd8d2d01d55b4bc43
Andreas Rid <info@andreas-rid.de>
2024-12-06 13:39:04 +0100

    flakes: nix package assignment has no effect and script 'nixFlakes' is not used - remote it
    This fixes the following error:
    error: 'nixFlakes' has been renamed to/replaced by 'nixVersions.stable
    
    remove nixFlakes commang

--- nixos/flakes.nix
+++ nixos/flakes.nix
@@ -4,15 +4,8 @@
 
 { pkgs, ... }: {
   nix = {
-    package = pkgs.nixFlakes; # or versioned attributes like nix_2_7
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
   };
-
-  environment.systemPackages = [
-    (pkgs.writeShellScriptBin "nixFlakes" ''
-      exec ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
-    '')
-  ];
 }
