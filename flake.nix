10eee9c693f09e25d6350722f93be26040fc8e6b
Andreas Rid <info@andreas-rid.de>
2025-08-21 17:56:24 +0200

    nixos upgrade 24.11 -> 25.05

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Nixos configuration";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-24.11;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-25.05;
     nur.url = github:nix-community/NUR;
     sops-nix.url = "github:Mic92/sops-nix";
     impermanence.url = "github:nix-community/impermanence";
 
     home-manager = {
-      url = github:nix-community/home-manager/release-24.11;
+      url = github:nix-community/home-manager/release-25.05;
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
