39a1d75ff7b4786e7b67b54498de6fb4a96b767c
Andreas Rid <info@andreas-rid.de>
2025-12-26 20:01:46 +0100

    fix out of memory: run garbage collection after a successful upgrade (delete generations older than 60d)

--- nixos/auto-upgrade.nix
+++ nixos/auto-upgrade.nix
@@ -14,11 +14,12 @@
     # Build  the  new  configuration  and make it the boot default, but do not activate it.
     # That is, the system continues to run the previous configuration until the next reboot.
     operation = "boot";
+    runGarbageCollection = true;
   };
 
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 120d";
+    options = "--delete-older-than 60d";
   };
 }
